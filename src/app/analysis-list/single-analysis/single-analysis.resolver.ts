5f1b2e1962ea3754bf24ca2c4560b0fb66998d81
Benjamin Speidel <b.speidel@1blick.de>
2019-01-12 11:57:43 +0100

    create analysis

---
+++ src/app/analysis-list/single-analysis/single-analysis.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { FirebaseService } from '../../services/firebase.service';
+
+@Injectable()
+export class SingleAnalysisResolver implements Resolve<any> {
+  constructor(public firebaseService: FirebaseService) {}
+
+  resolve(route: ActivatedRouteSnapshot) {
+    return new Promise((resolve, reject) => {
+      let customerId = route.paramMap.get('id');
+      this.firebaseService.getAnalysis(customerId).subscribe(data => {
+        resolve(data);
+      });
+    });
+  }
+}
